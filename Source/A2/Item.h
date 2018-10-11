67ddab79c4ed9e3f183ca51a386af3b99136c7c9
Harrison Verrios <atomicnitro5@gmail.com>
2018-10-12 01:39:41 +1100

    Create a health item pickup and allow it to rotate and play sound on collect.

---
+++ Source/A2/Item.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "Item.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class A2_API AItem : public AInteractable
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
